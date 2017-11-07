198d5dceea1674ff91af20be1175e05504a8a3ba
Jonathan Dickinson <me@jcdickinson.me>
2017-11-06 20:08:55 -0800

    Latest changes

--- TestJit/HashCode.cs
+++ TestJit/HashCode.cs
@@ -61,12 +61,18 @@ namespace System
         private const uint Prime5 = 374761393U;
 
         private uint _v1, _v2, _v3, _v4;
-        private uint _queue1, _queue2, _queue3;
+        private uint _queue0, _queue1, _queue2;
         private uint _length;
-        
-        [MethodImpl(MethodImplOptions.NoInlining)]
+
         public static int Combine<T1>(T1 value1)
         {
+            // Provide a way of diffusing bits from something with a  limited
+            // input hash space. For example, many enums only have a few
+            // possible hashes, only using the bottom few bits of the code. Some
+            // collections are built on the assumption that hashes are spread
+            // over a larger space, so diffusing the bits may help the
+            // collection work more efficiently.
+
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
 
             uint hash = MixEmptyState();
@@ -78,7 +84,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2>(T1 value1, T2 value2)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -94,7 +99,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3>(T1 value1, T2 value2, T3 value3)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -112,7 +116,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3, T4>(T1 value1, T2 value2, T3 value3, T4 value4)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -134,7 +137,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3, T4, T5>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -159,7 +161,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3, T4, T5, T6>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -186,7 +187,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3, T4, T5, T6, T7>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -215,7 +215,6 @@ namespace System
             return (int)hash;
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8)
         {
             var hc1 = (uint)(value1?.GetHashCode() ?? 0);
@@ -246,6 +245,206 @@ namespace System
             return (int)hash;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint Rol(uint value, int count)
+            => (value << count) | (value >> (32 - count));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void Initialize(out uint v1, out uint v2, out uint v3, out uint v4)
+        {
+            v1 = s_seed + Prime1 + Prime2;
+            v2 = s_seed + Prime2;
+            v3 = s_seed;
+            v4 = s_seed - Prime1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint Round(uint hash, uint input)
+        {
+            hash += input * Prime2;
+            hash = Rol(hash, 13);
+            hash *= Prime1;
+            return hash;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint QueueRound(uint hash, uint queuedValue)
+        {
+            hash += queuedValue * Prime3;
+            return Rol(hash, 17) * Prime4;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint MixState(uint v1, uint v2, uint v3, uint v4)
+        {
+            return Rol(v1, 1) + Rol(v2, 7) + Rol(v3, 12) + Rol(v4, 18);
+        }
+
+        private static uint MixEmptyState()
+        {
+            return s_seed + Prime5;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint MixFinal(uint hash)
+        {
+            hash ^= hash >> 15;
+            hash *= Prime2;
+            hash ^= hash >> 13;
+            hash *= Prime3;
+            hash ^= hash >> 16;
+            return hash;
+        }
+
+        public void Add<T>(T value)
+        {
+            Add(value?.GetHashCode() ?? 0);
+        }
+
+        public void Add<T>(T value, IEqualityComparer<T> comparer)
+        {
+            if (comparer is null)
+            {
+                Add(value);
+            }
+            else
+            {
+                Add(comparer.GetHashCode(value));
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Add(int value)
+        {
+            // The original xxHash works as follows:
+            // 0. Initialize immediately. We can't do this in a struct (no
+            //    default ctor).
+            // 1. Accumulate blocks of length 16 (4 uints) into 4 accumulators.
+            // 2. Accumulate remaining blocks of length 4 (1 uint) into the 
+            //    hash.
+            // 3. Accumulate remaining blocks of length 1 into the hash.
+
+            // There is no need for #3 as this type only accepts ints. _queue0,
+            // _queue1 and _queue2 are basically a buffer so that when 
+            // ToHashCode is called we can execute #2 correctly. That's what
+            // first three case statements do.
+
+            // We need to initialize the xxHash32 state (_v1 to _v4) lazily (see
+            // #0) nd the last place that can be done if you look at the
+            // original code is just before the first block of 16 bytes is mixed
+            // in. The xxHash32 state is never used for streams containing fewer
+            // than 16 bytes.
+
+            // To see what's really going on here, have a look at the Combine
+            // methods.
+            
+            var previousLength = _length++;
+            CheckLength(previousLength);
+
+            var val = (uint)value;
+            uint position = previousLength % 4;
+
+            // Binary search which will decrease the number of branches on
+            // average.
+
+            if (position % 2 == 0)
+            {
+                if (position == 0)
+                {
+                    _queue0 = val;
+                }
+                else
+                {
+                    System.Diagnostics.Debug.Assert(position == 2);
+                    _queue2 = val;
+                }
+                return;
+            }
+            else if (position == 1)
+            {
+                _queue1 = val;
+                return;
+            }
+
+            System.Diagnostics.Debug.Assert(position == 3);
+
+            if (previousLength == 4)
+                Initialize(out _v1, out _v2, out _v3, out _v4);
+
+            _v1 = Round(_v1, _queue0);
+            _v2 = Round(_v2, _queue1);
+            _v3 = Round(_v3, _queue2);
+            _v4 = Round(_v4, val);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void CheckLength(uint length)
+        {
+            // RyuJIT is currently unable to elide dead code that throws
+            // exceptions, so move this out of the method
+            // so tha the jitter can more aggressively optimize.
+            if (length == uint.MaxValue)
+                throw new InvalidOperationException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int ToHashCode()
+        {
+            uint length = _length;
+            uint position = length % 4;
+
+            // If the length is less than 4, _v1 to _v4 don't contain anything
+            // yet. xxHash32 treats this differently.
+
+            uint hash = length < 4
+                ? MixEmptyState()
+                : MixState(_v1, _v2, _v3, _v4);
+
+            // Multiply by 4 because we've been counting in bytes, not ints.
+
+            hash += length * 4;
+
+            // Mix what remains in the queue
+
+            // Switch can't be inlined right now, so emulate case statement
+            // fallthrough using goto.
+
+            // position refers to the *next* queue position in this method, so
+            // position == 1 means that _queue0 is populated; _queue1 would have
+            // been populated on the next call to Add.
+
+            if (position == 0) goto mixFinal;
+            hash = QueueRound(hash, _queue0);
+            if (position == 1) goto mixFinal;
+            hash = QueueRound(hash, _queue1);
+            if (position == 2) goto mixFinal;
+            hash = QueueRound(hash, _queue2);
+            
+            mixFinal:
+            hash = MixFinal(hash);
+            return (int)MixFinal(hash);
+        }
+
+#pragma warning disable 0809
+        // Obsolete member 'memberA' overrides non-obsolete member 'memberB'. 
+        // Disallowing GetHashCode is by design
+
+        // * We decided to not override GetHashCode() to produce the hash code 
+        //   as this would be weird, both naming-wise as well as from a behavioral 
+        //   standpoint (GetHashCode() should return the object's hash code, not 
+        //   the one being computed).
+
+        // * Even though ToHashCode() can be called safely multiple times on this
+        //   implementation, it is not part of the contract. If the implementation
+        //   has to change in the future we don't want to worry about people who
+        //   might have incorrectly used this type.
+
+        [Obsolete("Use ToHashCode to retrieve the computed hash code.", error: true)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => throw new NotSupportedException();
+#pragma warning restore 0809
+
+        
         public static volatile int _test;
 
         [MethodImpl(MethodImplOptions.NoInlining)]
@@ -339,184 +538,5 @@ namespace System
             hc.Add(_test);
             return hc.ToHashCode();
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint Rol(uint value, int count)
-            => (value << count) | (value >> (32 - count));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void Initialize(out uint v1, out uint v2, out uint v3, out uint v4)
-        {
-            v1 = s_seed + Prime1 + Prime2;
-            v2 = s_seed + Prime2;
-            v3 = s_seed + 0;
-            v4 = s_seed - Prime1;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint Round(uint hash, uint input)
-        {
-            hash += input * Prime2;
-            hash = Rol(hash, 13);
-            hash *= Prime1;
-            return hash;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint QueueRound(uint hash, uint queuedValue)
-        {
-            hash += queuedValue * Prime3;
-            return Rol(hash, 17) * Prime4;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint MixState(uint v1, uint v2, uint v3, uint v4)
-        {
-            return Rol(v1, 1) + Rol(v2, 7) + Rol(v3, 12) + Rol(v4, 18);
-        }
-
-        private static uint MixEmptyState()
-        {
-            return s_seed + Prime5;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint MixFinal(uint hash)
-        {
-            hash ^= hash >> 15;
-            hash *= Prime2;
-            hash ^= hash >> 13;
-            hash *= Prime3;
-            hash ^= hash >> 16;
-            return hash;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Add<T>(T value)
-        {
-            Add(value?.GetHashCode() ?? 0);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Add<T>(T value, IEqualityComparer<T> comparer)
-        {
-            if (comparer is null)
-                comparer = EqualityComparer<T>.Default;
-            Add(comparer.GetHashCode(value));
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void Add(int value)
-        {
-            // Note that x & 0x3 is like mod 3, but faster.
-
-            var val = (uint)value;
-            uint position = _length & 0x3;
-
-            // xxHash works as follows:
-            // 0. Initialize immediately. We can't do this in a struct (no default
-            //    ctor).
-            // 1. Accumulate blocks of length 16 (4 uints) into 4 accumulators.
-            // 2. Accumulate remaining blocks of length 4 (1 uint) into the hash.
-            // 3. Accumulate remaining blocks of length 1 into the hash.
-
-            // There is no need for *3 as this type only accepts ints. _queue1,
-            // _queue2 and _queue3 are basically a buffer so that when ToHashCode is
-            // called we can execute *2 correctly. That's what first three case
-            // statements do.
-
-            // We need to initialize the xxHash32 state (_v1 -> _v4) lazily (see *0)
-            // and the last place that can be done if you look at the original code
-            // is just before the first block of 16 bytes is mixed in. The xxHash32
-            // state is never used for streams containing fewer than 16 bytes.
-
-            // A bloom filter is used to determine whether the default case statement
-            // has even been executed. To do that we check if the length is smaller
-            // than 4 (_length ^ position will be non-zero if this is the case). The
-            // case statement is for values larger than 2, so the check only succeeds
-            // on exactly 3.
-
-            // To see what's really going on here, have a look at the Combine methods.
-
-            // Switch can't be inlined.
-
-            if (position == 0)
-                _queue1 = val;
-            else if (position == 1)
-                _queue2 = val;
-            else if (position == 2)
-                _queue3 = val;
-            else // == 3
-            {
-                // length smaller than 4?
-                if ((_length ^ position) == 0)
-                    Initialize(out _v1, out _v2, out _v3, out _v4);
-
-                _v1 = Round(_v1, _queue1);
-                _v2 = Round(_v2, _queue2);
-                _v3 = Round(_v3, _queue3);
-                _v4 = Round(_v4, val);
-            }
-
-            // Throw for more than uint.MaxValue fields.
-            _length = checked(_length + 1);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int ToHashCode()
-        {
-            uint position = _length & 0x3;
-
-            // If the length is less than 3, _v1 -> _v4 don't contain
-            // anything yet. xxHash32 treats this differently.
-
-            uint hash = (_length ^ position) == 0
-                ? MixEmptyState()
-                : MixState(_v1, _v2, _v3, _v4);
-
-            // Multiply by 4 because we've been counting in ints, not
-            // bytes.
-
-            hash += _length * 4;
-
-            // Mix what remains in the queue
-            // Switch can't be inlined right now, so use as few
-            // branches as possible instead.
-
-            if (position > 0)
-            {
-                hash = QueueRound(hash, _queue1);
-                if (position > 1)
-                {
-                    hash = QueueRound(hash, _queue2);
-                    if (position > 2)
-                        hash = QueueRound(hash, _queue3);
-                }
-            }
-
-            hash = MixFinal(hash);
-            return (int)hash;
-        }
-
-#       pragma warning disable 0809
-        // Obsolete member 'memberA' overrides non-obsolete member 'memberB'. 
-        // Disallowing GetHashCode is by design
-
-        // * We decided to not override GetHashCode() to produce the hash code 
-        //   as this would be weird, both naming-wise as well as from a behavioral 
-        //   standpoint (GetHashCode() should return the object's hash code, not 
-        //   the one being computed).
-
-        // * Even though ToHashCode() can be called safely multiple times on this
-        //   implementation, it is not part of the contract. If the implementation
-        //   has to change in the future we don't want to worry about people who
-        //   might have incorrectly used this type.
-
-        [Obsolete("Use ToHashCode to retrieve the computed hash code.", error: true)]
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public override int GetHashCode() => throw new NotSupportedException();
-#       pragma warning restore 0809
-
     }
 }
--- TestJit/dumpil.bat
+++ TestJit/dumpil.bat
@@ -1,19 +1,27 @@
 @echo off
+SET Methods=System.HashCode::Test_8
 SET AppDir=%CD%\bin\Release\netcoreapp2.0\win10-x64\
 SET CoreClrDir=C:\dev\coreclr\bin\Product\Windows_NT.x64.Debug
 SET OCD=%CD%
 
-dotnet publish -c Release > NUL
+dotnet publish -c Release
 robocopy /e %CoreClrDir% %AppDir%\publish > NUL
 
 SET CORE_ROOT=%CoreClrDir%
 SET CORE_LIBRARIES=%AppDir%publish\
 SET SAVESTAMP=%DATE:/=-%@%TIME::=-%
 SET SAVESTAMP=%SAVESTAMP: =%
-SET SAVESTAMP=%OCD%\%SAVESTAMP:,=.%.il
+
+SET SAVEDisasm=%OCD%\%SAVESTAMP:,=.%.asm
+SET SAVEDump=%OCD%\%SAVESTAMP:,=.%.jitdump
 
 cd %AppDir%
-rem set COMPLUS_JitDump=System.HashCode:Test
-set COMPlus_JitDisasm=System.HashCode:*
-%CoreClrDir%\corerun TestJit.dll > %SAVESTAMP%
-code %SAVESTAMP%
+
+SET COMPlus_JitDiffableDasm=1
+
+SET COMPlus_JitDisasm=%Methods%
+%CoreClrDir%\corerun TestJit.dll > %SAVEDisasm%
+SET COMPlus_JitDisasm=
+
+SET COMPLUS_JitDump=%Methods%
+%CoreClrDir%\corerun TestJit.dll > %SAVEDump%
